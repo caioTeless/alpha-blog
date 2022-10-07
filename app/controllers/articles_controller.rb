c0acb1577dfb0025328baf54119295c2f68c59bf
Caio Teles <caioo.teless@gmail.com>
2022-10-06 21:11:24 -0300

    Add one-to-may association between users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article] # it works sending a json to another page like PHP
         #@article = Article.new(params.require(:article).permit(:title, :description))
         @article = Article.new(article_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully."
         # render plain: @article.inspect
