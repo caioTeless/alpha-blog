8f60acba9d0f01b871fe28fdffc48a58b0fc5dd9
Caio Teles <caioo.teless@gmail.com>
2022-10-08 10:06:36 -0300

    Add profile and articles for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end 
+
     def new
         @user = User.new
     end 
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "The user has been updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
 
     def create
         @user = User.new(user_params)
